5233a93be7780d044262b7ade47075c0313ba5c3
Felipe Genuino <design@felipegenuino.com>
2016-04-22 00:52:17 -0300

    Structure commit

---
+++ assets/app.js
@@ -0,0 +1,44 @@
+// Using requirejs
+require(['libs/bower_components/Chart.js/dist/Chart.min.js'], function(Chart){
+    // Use Chart.js as normal here.
+
+    // Chart.noConflict restores the Chart global variable to it's previous owner
+    // The function returns what was previously Chart, allowing you to reassign.
+    var Chartjs = Chart.noConflict();
+
+});
+ 
+// Get context with jQuery - using jQuery's .get() method.
+var ctx = $("#myChart").get(0).getContext("2d");
+// This will get the first returned node in the jQuery collection.
+var myNewChart = new Chart(ctx);
+
+
+new Chart(ctx).PolarArea(data, options);
+
+var myLineChart = new Chart(ctx).Line(data, options);
+var data = {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [
+        {
+            label: "My First dataset",
+            fillColor: "rgba(220,220,220,0.2)",
+            strokeColor: "rgba(220,220,220,1)",
+            pointColor: "rgba(220,220,220,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(220,220,220,1)",
+            data: [65, 59, 80, 81, 56, 55, 40]
+        },
+        {
+            label: "My Second dataset",
+            fillColor: "rgba(151,187,205,0.2)",
+            strokeColor: "rgba(151,187,205,1)",
+            pointColor: "rgba(151,187,205,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(151,187,205,1)",
+            data: [28, 48, 40, 19, 86, 27, 90]
+        }
+    ]
+};
