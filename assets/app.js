32b1be8b479c8934bc37cf027c28d96173351625
Felipe Genuino <design@felipegenuino.com>
2016-04-22 09:57:50 -0300

    Update Chart

--- assets/app.js
+++ assets/app.js
@@ -1,44 +1,30 @@
-// Using requirejs
-require(['libs/bower_components/Chart.js/dist/Chart.min.js'], function(Chart){
-    // Use Chart.js as normal here.
-
-    // Chart.noConflict restores the Chart global variable to it's previous owner
-    // The function returns what was previously Chart, allowing you to reassign.
-    var Chartjs = Chart.noConflict();
-
-});
- 
-// Get context with jQuery - using jQuery's .get() method.
-var ctx = $("#myChart").get(0).getContext("2d");
-// This will get the first returned node in the jQuery collection.
-var myNewChart = new Chart(ctx);
+Chart.defaults.global.responsive = true;
 
 
-new Chart(ctx).PolarArea(data, options);
-
-var myLineChart = new Chart(ctx).Line(data, options);
 var data = {
     labels: ["January", "February", "March", "April", "May", "June", "July"],
     datasets: [
         {
-            label: "My First dataset",
-            fillColor: "rgba(220,220,220,0.2)",
-            strokeColor: "rgba(220,220,220,1)",
-            pointColor: "rgba(220,220,220,1)",
+            label: "Google Income",
+            fillColor: "rgba(229,89,52,0.2)",
+            strokeColor: "#9BC53D",
+            pointColor: "#E55934",
             pointStrokeColor: "#fff",
             pointHighlightFill: "#fff",
             pointHighlightStroke: "rgba(220,220,220,1)",
-            data: [65, 59, 80, 81, 56, 55, 40]
+            data: [65, 59, 40, 71, 56, 65, 80]
         },
         {
-            label: "My Second dataset",
+            label: "Ads Income",
             fillColor: "rgba(151,187,205,0.2)",
-            strokeColor: "rgba(151,187,205,1)",
-            pointColor: "rgba(151,187,205,1)",
+            strokeColor: "#E55934",
+            pointColor: "#02C39A",
             pointStrokeColor: "#fff",
             pointHighlightFill: "#fff",
             pointHighlightStroke: "rgba(151,187,205,1)",
-            data: [28, 48, 40, 19, 86, 27, 90]
+            data: [12, 16, 20, 15, 34, 43, 70]
         }
     ]
 };
+var context = document.getElementById("graph").getContext("2d");
+new Chart(context).Line(data);
